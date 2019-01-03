89f5b22133d8b45b78cd1516cf96cf7d3ac82bdd
jayeshkarvir5 <jayeshkarvir5@gmail.com>
2019-01-03 16:16:30 +0530

    additional files

---
+++ video-processing-py-cv2.py
@@ -0,0 +1,34 @@
+import cv2
+from darkflow.net.build import TFNet
+import numpy as np
+import time
+
+options = {
+    'model': 'cfg/yolo.cfg',
+    'load': 'bin/yolov2.weights',
+    'threshold': 0.15,
+    'gpu': 1.0
+}
+tfnet = TFNet(options)
+capture = cv2.VideoCapture("Imagine Dragons - Radioactive.mp4")
+colors = [tuple(255 * np.random.randn(3)) for i in range(5)]
+while(capture.isOpened):
+    stime = time.time()
+    ret, frame = capture.read()
+    results = tfnet.return_predict(frame)
+    if ret:  # if video is playing
+        for color, result in zip(colors, results):
+            tl = (result['topleft']['x'], result['topleft']['y'])
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            frame = cv2.rectangle(frame, tl, br, (0, 255, 0), 7)
+            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX,
+                                1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        print('FPS {:.1f}'.format(1 / time.time()-stime))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        capture.release()
+        cv2.destroyAllWindows()
+        break
